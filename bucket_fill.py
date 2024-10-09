72bf80d48dca347253ec2952472b8afbef9d1e41
sunitadhavale <sunitadhavale@gmail.com>
2024-10-09 11:45:11 +0100

    Python Coursework submission version1

--- bucket_fill.py
+++ bucket_fill.py
@@ -80,6 +80,90 @@ def show_image(image):
     print(stringify_image(image))
 
 
+def is_not_integer(seed_point):
+    ''' Check wether the seed_point has a non-integer coordinate
+
+    Args:
+        seed_point (tuple) :  a 2-element tuple representing the (row, col) 
+                       coordinates of the seed point to start filling
+    
+    Return:
+        bool : Wether the seed_pont is integer or not
+
+    '''
+
+    return type(seed_point[0]) != int or type(seed_point[1]) != int
+
+
+def is_outside_image(image, seed_point):
+    ''' Check wether the seed_point is outside the image or not
+
+    Args:
+        image (list) : a 2D nested list representation of an image, where
+                       0 represents an unfilled pixel, and
+                       1 represents a boundary pixel
+        seed_point (tuple) :  a 2-element tuple representing the (row, col) 
+                       coordinates of the seed point to start filling
+    
+    Return:
+        bool : Wether the seed_pont lies outside the image or not
+
+    '''
+    return seed_point[0]<0 or seed_point[1]<0 or (seed_point[0]>len(image)-1) or seed_point[1]>len(image[0])-1
+
+
+def is_boundary_pixel(image, seed_point):
+    ''' Check wether the seed_point is on a boundary pixel or not
+
+    Args:
+        image (list) : a 2D nested list representation of an image, where
+                       0 represents an unfilled pixel, and
+                       1 represents a boundary pixel
+        seed_point (tuple) :  a 2-element tuple representing the (row, col) 
+                       coordinates of the seed point to start filling
+    
+    Return:
+        bool : Wether the seed_pont is on a boundary pixel or not 
+
+    '''
+
+    return image[seed_point[0]][seed_point[1]] == 1
+
+
+def recursive_fill(image, row, col):
+    ''' 
+    Recursively fill the image from seed point to boundary
+
+    Args:
+        image (list) : a 2D nested list representation of an image, where
+                       0 represents an unfilled pixel, and
+                       1 represents a boundary pixel
+        seed_point (tuple) :  a 2-element tuple representing the (row, col) 
+                       coordinates of the seed point to start filling
+    
+    Return:
+        bool : Wether the seed_pont is on a boundary pixel or not 
+
+    '''
+
+    row_len = len(image)
+    col_len = len(image[0])
+
+    # Base condition: When the seed_point is outside the image, the position of seed_point is filled
+
+    if row>=row_len or row<0 or col>=col_len or col<0 or image[row][col]==2: 
+        return None
+
+    # Fill the adjacent pixels of the seed_point if the pixel is not a boundary pixel
+
+    if image[row][col] != 1:
+        image[row][col] = 2
+        recursive_fill(image, row, col+1)
+        recursive_fill(image, row, col-1)
+        recursive_fill(image, row+1, col)
+        recursive_fill(image, row-1, col)
+
+
 def fill(image, seed_point):
     """ Fill the image from seed point to boundary
 
@@ -103,21 +187,36 @@ def fill(image, seed_point):
     """
 
     # TODO: Complete this function
-    return []
+
+    row, col = seed_point
+
+    if is_not_integer(seed_point):
+        return image
+
+    if is_outside_image(image,seed_point):
+        return image
+
+    if is_boundary_pixel(image, seed_point):
+        return image
+
+    recursive_fill(image, row, col)
+
+    return image
+
 
 
 def example_fill():
-    image = load_image("data/bar.txt")
+    image = load_image("data/text_25.txt")
 
     print("Before filling:")
     show_image(image)
 
-    image = fill(image=image, seed_point=(7, 3))
+    image = fill(image=image, seed_point=(1, 10))
 
     print("-" * 25)
     print("After filling:")
     show_image(image)
-
+    print(image)
 
 if __name__ == '__main__':
     example_fill()
--- test_bucket_fill.py
+++ test_bucket_fill.py
@@ -1,8 +1,128 @@
-from bucket_fill import fill
+from bucket_fill import (
+    fill,
+    recursive_fill,
+    is_boundary_pixel, 
+    is_outside_image, 
+    is_not_integer, 
+    load_image
+)
+
+#Write test cases for all valid inputs
+
+def test_is_boundary_pixel():
+    '''To validate the code when the given seed_point lies on the boundary of the image '''
+
+    seed_point = (0, 1)
+    expected_image = load_image("data/smiley.txt")
+    filled_image = fill(expected_image, seed_point)
+
+    assert expected_image == filled_image
+
+
+def test_is_not_integer():
+    '''To validate the code when the given seed_point has a non-integer coordinates '''
+
+    seed_point = (1.3, "a")
+    expected_image = load_image("data/smiley.txt")
+    filled_image = fill(expected_image, seed_point)
+
+    assert expected_image == filled_image
+
+
+def test_is_outside_image():
+    '''To validate the code when the seed_point lies outside of the image '''
+
+    seed_point = (-1, 4)
+    expected_image = load_image("data/smiley.txt")
+    filled_image = fill(expected_image, seed_point)
+
+    assert expected_image == filled_image
+
+
+def test_valid_seedpoint():
+    '''To validate the code when the seed_point is valid '''
+
+    seed_point = (2, 4)
+    org_image = load_image("data/smiley.txt")
+    
+    expected_image = [[0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0], 
+                    [1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1], 
+                    [1, 2, 2, 1, 2, 2, 2, 1, 2, 2, 1], 
+                    [1, 2, 1, 0, 1, 2, 1, 0, 1, 2, 1], 
+                    [1, 2, 2, 1, 2, 2, 2, 1, 2, 2, 1], 
+                    [1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1], 
+                    [1, 2, 1, 1, 1, 1, 1, 1, 1, 2, 1], 
+                    [1, 2, 1, 0, 0, 0, 0, 0, 1, 2, 1], 
+                    [1, 2, 2, 1, 0, 0, 0, 1, 2, 2, 1], 
+                    [1, 2, 2, 2, 1, 1, 1, 2, 2, 2, 1], 
+                    [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0]]
+
+    filled_image = fill(org_image, seed_point)
+
+    assert expected_image == filled_image
+
+
+def test_single_pixel_image():
+    '''To validate the code when I have an image consisting of only one pixel '''
+
+    seed_point = (0, 0)
+    org_image = [[0]]
+
+    expected_image = [[2]]
+
+    filled_image = fill(org_image, seed_point)
+    assert  expected_image == filled_image
+
+
+def test_large_image():
+    '''To validate the code when I have a large image of size (25x25) '''
+
+    seed_point = (1, 10)
+    org_image = load_image("data/text_25.txt")
+    
+    expected_image = [[0, 0, 0, 0, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 1, 2, 1, 2, 1, 2], 
+                    [0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2], 
+                    [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2], 
+                    [0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2], 
+                    [0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2], 
+                    [0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2], 
+                    [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2], 
+                    [0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2], 
+                    [0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2], 
+                    [0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2], 
+                    [1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2], 
+                    [1, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 1, 0, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2], 
+                    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2], 
+                    [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2], 
+                    [0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2], 
+                    [0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2], 
+                    [0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2], 
+                    [0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2], 
+                    [0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2], 
+                    [0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2], 
+                    [0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2], 
+                    [0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2], 
+                    [0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2], 
+                    [0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2], 
+                    [0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2]]
+
+    filled_image = fill(org_image, seed_point)
+
+    assert expected_image == filled_image
 
 def test_pattern():
-    pass
+    
+    test_is_boundary_pixel()
 
+    test_is_not_integer()
+
+    test_is_outside_image()
+
+    test_valid_seedpoint()
+
+    test_single_pixel_image()
+
+    #test_large_image()
 
 if __name__ == '__main__':
     # This is just an example. Feel free to change this to whatever suits you.
