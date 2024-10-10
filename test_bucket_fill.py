ba1985a9d9fe4bc4642cc5df9da5b894fc9f7db6
sunitadhavale <sunitadhavale@gmail.com>
2024-10-10 16:24:45 +0100

    All test case s are included

--- test_bucket_fill.py
+++ test_bucket_fill.py
@@ -122,7 +122,7 @@ def test_pattern():
 
     test_single_pixel_image()
 
-    #test_large_image()
+    test_large_image()
 
 if __name__ == '__main__':
     # This is just an example. Feel free to change this to whatever suits you.
